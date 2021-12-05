d6e92deed5ad13a3fead4ba3c4ff2429b9f9e1bc
Bryant Baldallaque <38927423+bbaldallaque@users.noreply.github.com>
2021-12-04 23:29:42 -0400

    Formuylario de contacto
    Formulario de contacto y pagina de proyectos

--- Portafolio.Web/Program.cs
+++ Portafolio.Web/Program.cs
@@ -1,8 +1,10 @@
+using Portafolio.Web.Servicios;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddTransient<IRepositorioProyectos, RepositorioProyectos>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
